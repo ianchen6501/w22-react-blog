761bb4f8ec405806edf68eba992bfec3c7e48dc7
unknown <ianchen6501@gmail.com>
2020-12-17 00:32:58 +0800

    fix fbsdk

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
-import { ThemeProvider } from "styled-components"; //一個 <ThemeProvider>的外層 Component 來實踐換網站主題設計的系統
+import styled, { ThemeProvider } from "styled-components"; //一個 <ThemeProvider>的外層 Component 來實踐換網站主題設計的系統
 
 const theme = {
   colors : {
@@ -11,10 +11,91 @@ const theme = {
   }
 }
 
+function initFacebookSdk() {
+  return new Promise(resolve => {
+    console.log('init')
+    //初始化
+    window.fbAsyncInit = function() {
+      window.FB.init({
+        appId      : '382475983013781',
+        cookie     : true,
+        xfbml      : true,
+        version    : 'v9.0'
+      });
+      //記錄用戶行為資料 可在後台查看用戶資訊
+      window.FB.AppEvents.logPageView();   
+        
+    };
+    //嵌入臉書sdk
+    (function(d, s, id){
+       var js, fjs = d.getElementsByTagName(s)[0];
+       if (d.getElementById(id)) {return;}
+       js = d.createElement(s); js.id = id;
+       js.src = "https://connect.facebook.net/en_US/sdk.js";
+       fjs.parentNode.insertBefore(js, fjs);
+     }(document, 'script', 'facebook-jssdk'));
+  })
+}
+
+const startApp = () => {
+  console.log('getStatus')
+  window.FB.getLoginStatus(function(response) {
+    statusChangeCallback(response);
+  });
+
+  function testAPI() {
+    console.log('Welcome!  Fetching your information.... ');
+    window.FB.api('/me', function(response) {
+      console.log('Successful login for: ' + response.name);
+      document.getElementById('status').innerHTML =
+        'Thanks for logging in, ' + response.name + '!';
+    });
+
+  }
+  
+  function statusChangeCallback(response) {
+    console.log('statusChangeCallback');
+    console.log(response);
+    // The response object is returned with a status field that lets the
+    // app know the current login status of the person.
+    // Full docs on the response object can be found in the documentation
+    // for FB.getLoginStatus().
+    if (response.status === 'connected') {
+      // Logged into your app and Facebook.
+      testAPI();
+    } else if (response.status === 'not_authorized') {
+      // The person is logged into Facebook, but not your app.
+      document.getElementById('status').innerHTML = 'Please log ' +
+        'into this app.';
+    } else {
+      // The person is not logged into Facebook, so we're not sure if
+      // they are logged into this app or not.
+      document.getElementById('status').innerHTML = 'Please log ' +
+        'into Facebook.';
+      window.FB.login(function (response) {
+        if (response.authResponse) {
+          window.FB.api('/me',{fields: 'id,name,email'}, function (response) {
+            //這邊就可以判斷取得資料跟網站使用者資料是否一致
+          });
+        }
+      //FB.login()預設只會回傳基本的授權資料
+      //如果想取得額外的授權資料需要另外設定在scope參數裡面
+      //可以設定的授權資料可以參考官方文件          
+      }, { scope: 'email,user_likes' });
+        
+    }
+  }
+}
+
+initFacebookSdk()
+
+
+
 ReactDOM.render(
   <ThemeProvider theme={theme}>
       <App />
+      <button onClick={() => startApp()}>登入</button>
+      <div id={'status'}></div>
   </ThemeProvider>,
   document.getElementById('root')
 );
-
--- src/pages/LoginPage/LoginPage.js
+++ src/pages/LoginPage/LoginPage.js
@@ -52,103 +52,105 @@ export default function LoginPage() {
   const [errorMessage, setErrormessage] = useState('')
   const history = useHistory()
 
-  useLayoutEffect(() => {
-    const body = document.getElementsByTagName('body')[0]
-    const script = document.createElement('script')
-    script.innerHTML = `
-      window.fbAsyncInit = function() {
-        FB.init({
-          appId      : '382475983013781',
-          cookie     : true,
-          xfbml      : true,
-          version    : 'v9.0'
-        });
+  // useLayoutEffect(() => {
+  //   const body = document.getElementsByTagName('body')[0]
+  //   const script = document.createElement('script')
+  //   script.innerHTML = `
+  //     window.fbAsyncInit = function() {
+  //       FB.init({
+  //         appId      : '382475983013781',
+  //         cookie     : true,
+  //         xfbml      : true,
+  //         version    : 'v9.0'
+  //       });
           
-        FB.AppEvents.logPageView();   
+  //       FB.AppEvents.logPageView();   
           
-      };
+  //     };
 
-      (function(d, s, id){
-        var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) {return;}
-        js = d.createElement(s); js.id = id;
-        js.src = "https://connect.facebook.net/en_US/sdk.js";
-        fjs.parentNode.insertBefore(js, fjs);
-      }(document, 'script', 'facebook-jssdk'));
+  //     (function(d, s, id){
+  //       var js, fjs = d.getElementsByTagName(s)[0];
+  //       if (d.getElementById(id)) {return;}
+  //       js = d.createElement(s); js.id = id;
+  //       js.src = "https://connect.facebook.net/en_US/sdk.js";
+  //       fjs.parentNode.insertBefore(js, fjs);
+  //     }(document, 'script', 'facebook-jssdk'));
 
-      // FB.getLoginStatus(function(response) {
-      //   statusChangeCallback(response);
-      // });
+  //     // FB.getLoginStatus(function(response) {
+  //     //   statusChangeCallback(response);
+  //     // });
 
-      function getLoginStatus() {
-        FB.getLoginStatus(function(response) {
-          statusChangeCallback(response);
-        });
-      }
+  //     function getLoginStatus() {
+  //       FB.getLoginStatus(function(response) {
+  //         statusChangeCallback(response);
+  //       });
+  //     }
 
-      setTimeout(() => {
-        getLoginStatus()
-      }, 5000)
+  //     setTimeout(() => {
+  //       getLoginStatus()
+  //     }, 5000)
       
 
-      // This is called with the results from from FB.getLoginStatus().
-      function statusChangeCallback(response) {
-        console.log('statusChangeCallback');
-        console.log(response);
-        // The response object is returned with a status field that lets the
-        // app know the current login status of the person.
-        // Full docs on the response object can be found in the documentation
-        // for FB.getLoginStatus().
-        if (response.status === 'connected') {
-          // Logged into your app and Facebook.
-          testAPI();
-        } else if (response.status === 'not_authorized') {
-          // The person is logged into Facebook, but not your app.
-          document.getElementById('status').innerHTML = 'Please log ' +
-            'into this app.';
-        } else {
-          // The person is not logged into Facebook, so we're not sure if
-          // they are logged into this app or not.
-          document.getElementById('status').innerHTML = 'Please log ' +
-            'into Facebook.';
-        }
-      }
+  //     // This is called with the results from from FB.getLoginStatus().
+  //     function statusChangeCallback(response) {
+  //       console.log('statusChangeCallback');
+  //       console.log(response);
+  //       // The response object is returned with a status field that lets the
+  //       // app know the current login status of the person.
+  //       // Full docs on the response object can be found in the documentation
+  //       // for FB.getLoginStatus().
+  //       if (response.status === 'connected') {
+  //         // Logged into your app and Facebook.
+  //         testAPI();
+  //       } else if (response.status === 'not_authorized') {
+  //         // The person is logged into Facebook, but not your app.
+  //         document.getElementById('status').innerHTML = 'Please log ' +
+  //           'into this app.';
+  //       } else {
+  //         // The person is not logged into Facebook, so we're not sure if
+  //         // they are logged into this app or not.
+  //         document.getElementById('status').innerHTML = 'Please log ' +
+  //           'into Facebook.';
+  //       }
+  //     }
 
-      // This function is called when someone finishes with the Login
-      // Button.  See the onlogin handler attached to it in the sample
-      // code below.
-      function checkLoginState() {
-        FB.getLoginStatus(function(response) {
-          statusChangeCallback(response);
-        });
-      }
+  //     // This function is called when someone finishes with the Login
+  //     // Button.  See the onlogin handler attached to it in the sample
+  //     // code below.
+  //     function checkLoginState() {
+  //       FB.getLoginStatus(function(response) {
+  //         statusChangeCallback(response);
+  //       });
+  //     }
 
-      // Here we run a very simple test of the Graph API after login is
-      // successful.  See statusChangeCallback() for when this call is made.
-      function testAPI() {
-        console.log('Welcome!  Fetching your information.... ');
-        FB.api('/me', function(response) {
-          console.log('Successful login for: ' + response.name);
-          document.getElementById('status').innerHTML =
-            'Thanks for logging in, ' + response.name + '!';
-        });
+  //     // Here we run a very simple test of the Graph API after login is
+  //     // successful.  See statusChangeCallback() for when this call is made.
+  //     function testAPI() {
+  //       console.log('Welcome!  Fetching your information.... ');
+  //       FB.api('/me', function(response) {
+  //         console.log('Successful login for: ' + response.name);
+  //         document.getElementById('status').innerHTML =
+  //           'Thanks for logging in, ' + response.name + '!';
+  //       });
 
-      }
-
-      function login() {
-        FB.login(function (response) {
-          /*console.log(response);*/
-        });
-      }
+  //     }
       
-      function logout() {
-        FB.logout(function (response) {
-          /*console.log(response)*/
-        });
-      }
-    ` 
-    body.appendChild(script)
-  })
+  //     function FBlogin() {
+  //       FB.login(function (response) {
+  //         /*console.log(response);*/
+  //       });
+  //     }
+
+  //     function FBlogout() {
+  //       FB.logout(function (response) {
+  //         /*console.log(response)*/
+  //       });
+  //     }
+
+  //     console.log('hi')
+  //   ` 
+  //   body.prepend(script)
+  // })
 
   const handleSubmit = (event) => {
     setErrormessage(null)
@@ -184,7 +186,7 @@ export default function LoginPage() {
         <Button>登入</Button>
       </form>
       { errorMessage && <ErrorMessage>{errorMessage}</ErrorMessage> }
-      <button onClick={() => login()}>登入</button>
+      <button>登入</button>
       {/* <button onclick={() => logout()}>登出</button> */}
       <div id="status"></div>
     </Container>
